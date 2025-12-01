545030c07491337eb0ed69efd96438c889460434
Vladyslav Halimskyi <30364795+khero691@users.noreply.github.com>
2025-12-01 16:38:55 +0100

    Fix: Set correct rootDir and include shared types for Render build

--- dist/server/src/game/BotAI.d.ts
+++
@@ -1,8 +0,0 @@
-import { GameState, BattleAction, Team } from '../../../shared/types';
-import { BattleManager } from './BattleManager';
-export declare class BotAI {
-    private battleManager;
-    constructor(battleManager: BattleManager);
-    calculateMove(gameState: GameState, botTeam: Team): BattleAction;
-}
-//# sourceMappingURL=BotAI.d.ts.map
